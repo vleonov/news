bc6808dfb1849729ac29a7848157604f6e620e34
Vitaly Leonov <vleonov@avito.ru>
2015-02-24 23:43:22 +0300

    Cron for reset user coeff

--- bin/cron/resetCoeff.php
+++ bin/cron/resetCoeff.php
@@ -8,5 +8,5 @@ Bootstrap::run($configFilename);
 
 $db = Database::get();
 
-$db->exec('update news_users_coeff set coeff=GREATEST(coeff-0.1, 0) where coeff>0');
-$db->exec('update news_users_coeff set coeff=LEAST(coeff+0.1, 0) where coeff<0');
+$db->exec('update news_users_coeff set coeff=GREATEST(coeff-1, 0) where coeff>0');
+$db->exec('update news_users_coeff set coeff=LEAST(coeff+1, 0) where coeff<0');
